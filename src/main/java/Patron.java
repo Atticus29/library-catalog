1f68f3344240d2e8e8bde924a12f04d577bcc364
Mark Fisher <mark@>
2017-04-03 15:58:14 -0700

    add preliminary getters, save, equals, find, all methods and tests

--- src/main/java/Patron.java
+++ src/main/java/Patron.java
@@ -1,9 +1,83 @@
 import org.sql2o.*;
+import java.util.List;
+import java.sql.Timestamp;
+import java.util.Timer;
+import java.util.TimerTask;
+
 
 public class Patron {
+  private String name;
+  private String address;
+  private String phone;
+  private String email;
 
-  public Patron() {
 
+  public Patron(String name, String address, String phone, String email) {
+    this.name = name;
+    this.address = address;
+    this.phone = phone;
+    this.email = email;
+  }
+
+  public String getName(){
+    return this.name;
+  }
+
+  public String getAddress(){
+    return this.address;
+  }
+
+  public String getPhone(){
+    return this.phone;
+  }
+
+  public String getEmail(){
+    return this.email;
+  }
+
+  @Override
+  public boolean equals(Object otherPatron) {
+    if(!(otherPatron instanceof Patron)) {
+      return false;
+    } else {
+      Patron newPatron = (Patron) otherPatron;
+      return this.getName().equals(newPatron.getName())
+        && this.getAddress().equals(newPatron.getAddress()) &&
+        this.getPhone().equals(newPatron.getPhone()) &&
+        this.getEmail().equals(newPatron.getEmail());
+    }
+  }
+
+  public void save(){
+    String sqlCommand = "INSERT INTO patrons (name, address, phone, email) VALUES (:name, :address, :phone, :email)";
+    try(Connection con = DB.sql2o.open()){
+      this.id = (int) con.createQuery(sqlCommand, true)
+        .addParameter("name", this.name)
+        .addParameter("address", this.address)
+        .addParameter("phone", this.phone)
+        .addParameter("email", this.email)
+        .executeUpdate()
+        .getKey();
+    }
+  }
+
+  public static List<Patron> all(){
+    String sqlCommand = "SELECT * FROM patrons;";
+    try(Connection con = DB.sql2o.open()){
+      List<Patron> results = con.createQuery(sqlCommand)
+        .executeAndFetch(Patron.class);
+      return results;
+    }
+  }
+
+  public static Patron find(int id){
+    String sqlCommand = "SELECT * FROM patrons WHERE id=:id;";
+    try(Connection con = DB.sql2o.open()){
+      Patron result = con.createQuery(sqlCommand)
+        .addParameter("id", id)
+        .executeAndFetchFirst(Patron.class);
+      return result;
+    }
   }
 
 }
--- src/test/java/PatronTest.java
+++ src/test/java/PatronTest.java
@@ -3,18 +3,54 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class PatronTest {
+  private Patron testPatron;
 
   @Before
   public void setUp() {
-    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/library_catalog_test", null, null);
+    testPatron = new Patron("Ryan Murphy", "123 Maple Lane Portland, OR 97203", "5046179123", "ryan.murphy@gmail.com");
+    // testPatron.save();
   }
 
-  @After
-  public void tearDown() {
-    try (Connection con = DB.sql2o.open()) {
-      String sql = "DELETE FROM name_of_your_table *;";
-      con.createQuery(sql).executeUpdate();
-    }
+  @Test
+  public void Patron_instantiatesCorrectly_true() {
+    assertTrue(testPatron instanceof Patron);
   }
+
+  @Test
+  public void Patron_instantiatesWithNameAddressPhoneEmail_String() {
+    assertEquals("Ryan Murphy", testPatron.getName());
+    assertEquals("123 Maple Lane Portland, OR 97203", testPatron.getAddress());
+    assertEquals("5046179123", testPatron.getPhone());
+    assertEquals("ryan.murphy@gmail.com", testPatron.getEmail());
+  }
+
+  @Test
+  public void equals_returnsTrueIfNameAndPersonIdAreSame_true() {
+    Patron anotherPatron = new Patron("Bubbles", 1);
+    assertTrue(testPatron.equals(anotherPatron));
+  }
+
+  @Test
+  public void save_assignsIdToPatron_true(){
+    Patron savedPatron = Patron.all().get(0);
+    assertEquals(savedPatron.getId(), testPatron.getId());
+  }
+
+  @Test
+  public void all_returnsAllInstancesOfPatron_true(){
+    Patron secondPatron = new Patron ("Spud", 1);
+    secondPatron.save();
+    assertTrue(Patron.all().get(0).equals(testPatron));
+    assertTrue(Patron.all().get(1).equals(secondPatron));
+  }
+
+  @Test
+  public void find_returnsPatronWithSameId_secondPatron(){
+    Patron secondPatron = new Patron("Spud", 3);
+    secondPatron.save();
+    assertEquals(Patron.find(secondPatron.getId()), secondPatron);
+  }
+
+
 
 }
